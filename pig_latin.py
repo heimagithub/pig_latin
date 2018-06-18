8adb5fc705dcd6056978bf03471c31e3141798c1
heimagithub <heimagooogle@gmail.com>
2018-06-18 20:02:49 +0800

    first commit

---
+++ pig_latin.py
@@ -0,0 +1,133 @@
+# coding: utf-8
+from __future__ import print_function
+
+import os
+import re
+import string
+from itertools import dropwhile
+
+import numpy as np
+from keras.layers.recurrent import GRU
+from keras.layers.wrappers import TimeDistributed
+from keras.models import Sequential
+from keras.layers.core import Dense, RepeatVector
+
+# from consts import DATA_PATH
+from utils import build_model_from_file, save_model_to_file
+
+WORDS_FILE = 'words.txt'
+BEGIN_SYMBOL = '^'
+END_SYMBOL = '$'
+CHAR_SET = set(string.ascii_lowercase + BEGIN_SYMBOL + END_SYMBOL)
+CHAR_NUM = len(CHAR_SET)
+CHAR_TO_INDICES = {c: i for i, c in enumerate(CHAR_SET)}
+INDICES_TO_CHAR = {i: c for c, i in CHAR_TO_INDICES.items()}
+MAX_INPUT_LEN = 18
+MAX_OUTPUT_LEN = 20
+
+NON_ALPHA_PAT = re.compile('[^a-z]')
+
+
+def is_vowel(char):
+    return char in ('a', 'e', 'i', 'o', 'u')
+
+
+def is_consonant(char):
+    return not is_vowel(char)
+
+
+def pig_latin(word):
+    if is_vowel(word[0]):
+        return word + 'yay'
+    else:
+        remain = ''.join(dropwhile(is_consonant, word))
+        removed = word[:len(word) - len(remain)]
+        return remain + removed + 'ay'
+
+
+def vectorize(word, seq_len, vec_size):
+    vec = np.zeros((seq_len, vec_size), dtype=int)
+    for i, ch in enumerate(word):
+        vec[i, CHAR_TO_INDICES[ch]] = 1
+
+    for i in range(len(word), seq_len):
+        vec[i, CHAR_TO_INDICES[END_SYMBOL]] = 1
+
+    return vec
+
+
+def build_data():
+    words_file = os.path.join('/home/heima/pig_latin/data/', WORDS_FILE)
+    words = [
+        w.lower().strip() for w in open(words_file, 'r').readlines()
+        if w.strip() != '' and not NON_ALPHA_PAT.findall(w.lower().strip())
+    ]
+
+    plain_x = []
+    plain_y = []
+    for w in words:
+        plain_x.append(BEGIN_SYMBOL + w)
+        plain_y.append(BEGIN_SYMBOL + pig_latin(w))
+
+    # train_x 和 train_y 必须是 3-D 的数据
+    train_x = np.zeros((len(words), MAX_INPUT_LEN, CHAR_NUM), dtype=int)
+    train_y = np.zeros((len(words), MAX_OUTPUT_LEN, CHAR_NUM), dtype=int)
+    for i in range(len(words)):
+        train_x[i] = vectorize(plain_x[i], MAX_INPUT_LEN, CHAR_NUM)
+        train_y[i] = vectorize(plain_y[i], MAX_OUTPUT_LEN, CHAR_NUM)
+
+    return train_x, train_y
+
+
+def build_model(input_size, seq_len, hidden_size):
+    """建立一个 sequence to sequence 模型"""
+    model = Sequential()
+    model.add(GRU(input_dim=input_size, output_dim=hidden_size, return_sequences=False))
+    model.add(Dense(hidden_size, activation="relu"))
+    model.add(RepeatVector(seq_len))
+    model.add(GRU(hidden_size, return_sequences=True))
+    model.add(TimeDistributed(Dense(output_dim=input_size, activation="linear")))
+    model.compile(loss="mse", optimizer='adam')
+
+    return model
+
+
+def train(epoch, model_path):
+    x, y = build_data()
+    indices = int(len(x) / 10)
+    test_x = x[:indices]
+    test_y = y[:indices]
+    train_x = x[indices:]
+    train_y = y[indices:]
+
+    model = build_model(CHAR_NUM, MAX_OUTPUT_LEN, 128)
+
+    model.fit(train_x, train_y, validation_data=(test_x, test_y), batch_size=128, nb_epoch=epoch)
+
+    model_file = os.path.join(model_path, 'pig_latin.model')
+    save_model_to_file(model, model_file)
+
+
+def test(model_path, word):
+    model_file = os.path.join(model_path, 'pig_latin.model')
+    model = build_model_from_file(model_file)
+
+    x = np.zeros((1, MAX_INPUT_LEN, CHAR_NUM), dtype=int)
+    word = BEGIN_SYMBOL + word.lower().strip() + END_SYMBOL
+    x[0] = vectorize(word, MAX_INPUT_LEN, CHAR_NUM)
+
+    pred = model.predict(x)[0]
+    print(''.join([
+        INDICES_TO_CHAR[i] for i in pred.argmax(axis=1)
+        if INDICES_TO_CHAR[i] not in (BEGIN_SYMBOL, END_SYMBOL)
+    ]))
+
+
+# if __name__ == "__main__":
+#     print('start to train')
+#     train(20, '/home/heima/pig_latin/')
+
+
+if __name__ == "__main__":
+    print('start to test')
+    test('/home/heima/pig_latin/', 'angry')
---
+++ utils.py
@@ -0,0 +1,129 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+import re
+import pickle
+import jieba
+import logging
+from functools import partial, reduce
+
+from keras.models import Sequential
+
+jieba.setLogLevel(logging.INFO)
+
+PUNCTS_PATTERN = re.compile(r"[.,;:!?'\"~\[\]\(\)\{\}_—。…．，；、：！？‘’“”〕《》【】〖〗（）「」～]")
+SPACES_PATTERN = re.compile(r"[\r\n\t\u00a0 ]")
+SENT_SEP = u'。，！？～；：.,!?:;'
+
+
+def to_halfwidth(text):
+    """将文本中的全角字符转换为半角字符"""
+    res = ''
+    for char in text:
+        inside_code = ord(char)
+        if inside_code == 0x3000:
+            inside_code = 0x0020
+        else:
+            inside_code -= 0xfee0
+
+        if inside_code < 0x0020 or inside_code > 0x7e:
+            res += char
+        else:
+            res += chr(inside_code)
+
+    return res
+
+
+def remove_punctuations(text):
+    """从文本中移除标点符号"""
+    return PUNCTS_PATTERN.sub(' ', text)
+
+
+def unify_whitespace(text):
+    """统一文本中的空白字符为空格"""
+    return SPACES_PATTERN.sub(' ', text)
+
+
+def remove_redundant(text, chars):
+    """将字符串中连续的指定字符压缩成一个"""
+    if chars == '' or text == '':
+        return text
+
+    char_set = set(chars)
+    prev = ''
+    result = ''
+    for ch in text:
+        if ch != prev or ch not in char_set:
+            result += ch
+
+        prev = ch
+
+    return result
+
+
+def clean(text):
+    funcs = [
+        to_halfwidth,
+        remove_punctuations,
+        unify_whitespace,
+        partial(remove_redundant, chars=u' ')
+    ]
+    cleaned_text = reduce(lambda x, fn: fn(x), [text] + funcs)
+    return cleaned_text
+
+
+def words_tokenize(text):
+    """分词"""
+    return [word.strip() for word in jieba.cut(text) if len(word.strip()) > 0]
+
+
+def sents_tokenize(text, puncts=SENT_SEP):
+    """分句"""
+    tokens = words_tokenize(text)
+    sents = []
+
+    prev = u' '
+    cur_sent = []
+    for tk in tokens:
+        if tk not in puncts and prev in puncts:
+            sents.append(cur_sent)
+            cur_sent = []
+
+        cur_sent.append(tk)
+        prev = tk
+
+    if cur_sent:
+        sents.append(cur_sent)
+
+    return sents
+
+
+def shingle(sequence, length):
+    if len(sequence) < length:
+        return []
+    else:
+        return [sequence[i:i + length] for i in range(len(sequence) - length + 1)]
+
+
+def build_model_from_file(model_file):
+    # a,b,c = pickle.load(open(model_file, 'rb'))
+    print(model_file)
+
+    structure, weights = pickle.load(open(model_file, 'rb'))
+    # print("XXXXXXXXXXXXXXXX"+str(structure))
+
+    model = Sequential.from_config(structure)
+    # print("YYYYYYYYYYYYYYy")
+
+    model.set_weights(weights)
+    # print("zzzzzzzzzzzzzzz")
+    return model
+
+
+def save_model_to_file(model, model_file):
+    # save model structure
+    structure = model.get_config()
+    weights = model.get_weights()
+    pickle.dump((structure, weights), open(model_file, 'wb'))
+
+
